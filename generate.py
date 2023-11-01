b1742a8d16b23da0f0f8fee8696cf72f5cbc37b9
Aleksandr <some_mail@mail.com>
2023-11-01 22:01:43 +0200

    load

--- generate.py
+++ generate.py
@@ -1,13 +1,24 @@
 from random import choice
-secret_word = None
-def generate_word():
-    vocabulary = dict()
 
-    with open('words.txt', 'r', encoding='utf-8') as file:
-        for i in range(100):
-            text = file.readline().split('-')
-            if len(text[0]) <= 12:
-                vocabulary[text[0].strip().upper()] = text[1].strip()
+secret_word = None
+
+def generate_word():
+    """
+    generate secret word from words_uk.txt
+
+    secret_word = random words
+    vocabulary - key: secret word, value: clue from secret word
+    number_words - words_uk.txt stores 25 pairs
+    word - sheet consistig of underscores. number underscors == len(secret_word)
+    clue - hint for the player
+    """
+    vocabulary = dict()
+    number_words = 25
+
+    with open('words_uk.txt', 'r', encoding='utf-8') as file:
+        for i in range(number_words):
+            text = file.readline().strip().split(' - ')
+            vocabulary[text[0].upper()] = text[1]
         secret_word = (choice(list(vocabulary.keys())))
     clue = vocabulary[secret_word]
     word = ['_' for i in range(len(secret_word))]
@@ -16,4 +27,5 @@ def generate_word():
 
 
 if __name__ == "__main__":
-    print(generate_word())
+    print(*generate_word(), sep='\n')
+
--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 import pygame
-from random import choice
-from pygame.color import THECOLORS as col
 from generate import *
 import sys
 
 pygame.init()
 clock = pygame.time.Clock()
+WIDTH = 800
+HEIGHT = 490
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+
 start = 0
-screen = pygame.display.set_caption('GALLOWS')
-screen = pygame.display.set_mode((800, 490))
+screen = pygame.display.set_caption('HangMan')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 font_1 = pygame.font.SysFont('italic', 40)
-font_2 = pygame.font.SysFont('italic', 100)
-font_3 = pygame.font.SysFont('italic', 30)
-font_4 = pygame.font.SysFont('italic', 80)
+font_2 = pygame.font.SysFont('italic', 120)
+font_3 = pygame.font.SysFont('italic', 80)
 
 back_ground = pygame.image.load(f'image/image_0{start}.jpg')
 
 input_box = pygame.Rect(10, 200, 100, 40)
 
-button_OK_rect = pygame.Rect(10, 250, 100, 70)
-button_OK_text = font_1.render('ОК', True, col['black'])
+btn_ok = pygame.Rect(10, 250, 100, 70)
+btn_ok_text = font_1.render('ОК', True, BLACK)
 
-button_GAME_rect = pygame.Rect(300, 320, 200, 70)
-button_GAME_text = font_4.render('Играть', True, col['black'])
+btn_game = pygame.Rect(300, 320, 200, 70)
+btn_game_text = font_3.render('Грати', True, BLACK)
 
 game = True
 user_input = ''
 tries = 7
 secret_word = None
 message = 0
-break_out = False
 entered_let = []
 messages = [
     ' ',                             
-    'Нужно ввести букву или слово!',                 
-    'Осталось попыток: ',           
+    'Потррібно ввести одну літеру або слово цілком!',                 
+    'Залишилось спроб: ',           
     'GAME OVER',                    
-    'Ты уже угадал эту букву!',     
+    'Ця літера вже була',     
     'CONGRATULATIONS',
 ]
+
 def check(letter):
     if not letter.isalpha():
         return False
@@ -51,17 +56,15 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if button_GAME_rect.collidepoint(event.pos) and not game:
+            if btn_game.collidepoint(event.pos) and not game:
                 game = True
                 tries = 7
                 start, message = 0, 0
                 clue, word, secret_word = generate_word()
                 entered_let = []
-                screen.blit(back_ground, (0, 0))
-            if button_OK_rect.collidepoint(event.pos):
-                print(start)
+            if btn_ok.collidepoint(event.pos):
                 message = 0
-                if user_input in word:
+                if user_input in word or user_input in entered_let:
                     message = 4
                     user_input = ''
                     continue
@@ -69,26 +72,23 @@ while True:
                     message = 1
                     user_input = ''
                     continue
+                if user_input not in entered_let:
+                    entered_let.append(user_input)
                 if len(user_input) == 1:
                     letter = [i for i, char in enumerate(secret_word.upper()) if char == user_input]
                     if letter:
                         for num in letter:
                             word[num] = user_input
                         if '_' not in word:
-                            message = 5
                             game = False
                             start, message = 0, 5
                     else:
                         start += 1; tries -= 1
-                        # tries -= 1
-
                 else:
                     if user_input == secret_word:
-                        message = 5
                         game = False
                         start, message = 0, 5
                     start += 1; tries -= 1
-                    # tries -= 1
                 if not tries:
                     game = False
                     start = 7
@@ -99,57 +99,56 @@ while True:
                 user_input = user_input[:-1]
             else:
                 user_input += event.unicode.upper()
-                if user_input not in entered_let:
-                    entered_let.append(user_input)
+            
 
     clock.tick(20)
     screen.blit(back_ground, (0, 0))
 
-    entered_letters = font_3.render(' '.join(entered_let), True, col['blue'])
-    entered_rect = entered_letters.get_rect(left=10, top=5) 
+    entered_text = font_1.render('*'.join(entered_let), True, BLUE)
+    entered_rect = entered_text.get_rect(left=10, top=40) 
 
-    secret_text = font_4.render(' '.join(word), True, col['black'])
-    secret_rect = secret_text.get_rect(center=((800 // 2, 150)))
+    secret_text = font_3.render(' '.join(word), True, BLACK)
+    secret_rect = secret_text.get_rect(center=((WIDTH // 2, 150)))
 
-    over_text = font_2.render(messages[3], True, col['red'])
-    over_rect = over_text.get_rect(center=((800 // 2), (490 / 2)))
+    over_text = font_2.render(messages[3], True, RED)
+    over_rect = over_text.get_rect(center=((WIDTH // 2), (HEIGHT // 2)))
 
-    win_text = font_2.render(messages[5], True, col['green'])
-    win_rect = win_text.get_rect(center=((800 // 2), (490 / 2)))
+    win_text = font_2.render(messages[5], True, GREEN)
+    win_rect = win_text.get_rect(center=((WIDTH // 2), (HEIGHT // 2)))
 
-    tries_text = font_3.render(f'{messages[2]} {tries}', True, col['black'])
-    tries_rect = tries_text.get_rect(left=10, top=400)
+    try_text = font_1.render(f'{messages[2]} {tries}', True, BLACK)
+    try_rect = try_text.get_rect(left=10, top=400)
   
-    warning_text = font_3.render(messages[message], True, col['red'])
-    warning_rect = warning_text.get_rect(left=5, top=350)
+    warning_text = font_1.render(messages[message], True, RED)
+    warning_rect = warning_text.get_rect(center=(WIDTH // 2, 80))
 
-    text_clue = font_1.render(clue, True, col['black'])
-    text_rect = text_clue.get_rect(left=20, top=30)
+    clue_text = font_1.render(clue, True, BLACK)
+    clue_rect = clue_text.get_rect(center=(WIDTH // 2, 20))
        
-    pygame.draw.rect(screen, col['yellow'], button_OK_rect)
-    pygame.draw.rect(screen, col['blue'], button_OK_rect, 4)
-    text_button_OK = button_OK_text.get_rect(center=(button_OK_rect.center))
+    pygame.draw.rect(screen, YELLOW, btn_ok)
+    pygame.draw.rect(screen, BLUE, btn_ok, 4)
+    text_in_btn_ok = btn_ok_text.get_rect(center=(btn_ok.center))
 
-    text_surface = font_1.render(user_input, True, col['black'])
-    width = max(100, text_surface.get_width()+10)
+    input_text = font_1.render(user_input, True, BLACK)
+    width = max(100, input_text.get_width()+10)
     input_box.w = min(width, 300)
-    pygame.draw.rect(screen, col['blue'], input_box, 4)
+    pygame.draw.rect(screen, BLUE, input_box, 4)
     
     if not game:
         screen.blit(pygame.image.load(f'image/image_0{start}.jpg'), (0, 0)) 
-        pygame.draw.rect(screen, col['green'], button_GAME_rect)
-        pygame.draw.rect(screen, col['black'], button_GAME_rect, 4)
-        text_button_GAME = button_GAME_text.get_rect(center=(button_GAME_rect.center))
+        pygame.draw.rect(screen, GREEN, btn_game)
+        pygame.draw.rect(screen, BLACK, btn_game, 4)
+        text_in_btn_game = btn_game_text.get_rect(center=(btn_game.center))
 
         screen.blit(over_text if not tries else win_text, over_rect if not tries else win_rect)
-        screen.blit(button_GAME_text, text_button_GAME)                                                                  
+        screen.blit(btn_game_text, text_in_btn_game)                                                       
     else:                                                           
-        screen.blit(entered_letters, entered_rect)
-        screen.blit(tries_text, tries_rect)                             
+        screen.blit(entered_text, entered_rect)
+        screen.blit(try_text, try_rect)                             
         screen.blit(warning_text, warning_rect)                         
-        screen.blit(text_surface, (input_box.x+5, input_box.y+5))      
-        screen.blit(text_clue, text_rect)                               
-        screen.blit(button_OK_text, text_button_OK)                      
+        screen.blit(input_text, (input_box.x+5, input_box.y+5))      
+        screen.blit(clue_text, clue_rect)                               
+        screen.blit(btn_ok_text, text_in_btn_ok)                      
         screen.blit(secret_text, secret_rect)                           
     pygame.display.flip()
 
